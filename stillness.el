820d0fcf0c2e970c05ff2c2dca7ce0fd98e69b5f
neeasade <neeasade@users.noreply.github.com>
2025-01-24 15:35:14 -0600

    indent

--- stillness.el
+++ stillness.el
@@ -29,46 +29,46 @@
 
 (defun stillness--minibuffer-height ()
   (or stillness-minibuffer-height
-      (and (bound-and-true-p vertico-mode) vertico-count)
-      (and (bound-and-true-p ivy-mode) ivy-height)
-      10))
+    (and (bound-and-true-p vertico-mode) vertico-count)
+    (and (bound-and-true-p ivy-mode) ivy-height)
+    10))
 
 (defun stillness--handle-point (read-call &rest args)
   (let ((minibuffer-count (stillness--minibuffer-height))
-	(minibuffer-offset 4)) 		; point adjustment above minibuffer
+         (minibuffer-offset 4)) ; point adjustment above minibuffer
     (save-window-excursion
       ;; delete any windows south of where the minibuffer will be:
       (->> (window-list)
-	   (-filter (lambda (w)
-		      (-let (((_ top _ _) (window-edges w)))
-			(> (1+ top) minibuffer-count))))
-	   (-map 'delete-window))
+        (-filter (lambda (w)
+                   (-let (((_ top _ _) (window-edges w)))
+                     (> (1+ top) minibuffer-count))))
+        (-map 'delete-window))
 
       ;; don't resize northern neighbor:
       (when-let (north (windmove-find-other-window 'up))
-	;; NB: save-window-excursion does not undo this 😩
-	(window-preserve-size north nil t))
+        ;; NB: save-window-excursion does not undo this 😩
+        (window-preserve-size north nil t))
 
       ;; move the point in any affected windows:
       (save-mark-and-excursion
-	(->> (window-list)
-	     (-map (lambda (window)
-		     (with-selected-window window
-		       (-let ((frame-line (+ (nth 1 (window-edges))
-					     (count-lines (window-start) (point)))))
-			 (when (> frame-line (- minibuffer-count minibuffer-offset))
-			   (deactivate-mark)
-			   (move-to-window-line
-			    (- minibuffer-count minibuffer-offset (nth 1 (window-edges))))))))))
+        (->> (window-list)
+          (-map (lambda (window)
+                  (with-selected-window window
+                    (-let ((frame-line (+ (nth 1 (window-edges))
+                                         (count-lines (window-start) (point)))))
+                      (when (> frame-line (- minibuffer-count minibuffer-offset))
+                        (deactivate-mark)
+                        (move-to-window-line
+                          (- minibuffer-count minibuffer-offset (nth 1 (window-edges))))))))))
 
-	;; do the thing:
-	(apply read-call args)))))
+        ;; do the thing:
+        (apply read-call args)))))
 
 (define-minor-mode stillness-mode
-    "Global minor mode to prevent windows from jumping on minibuffer activation."
+  "Global minor mode to prevent windows from jumping on minibuffer activation."
   :global t
   (if stillness-mode
-      (advice-add 'completing-read :around #'stillness--handle-point)
+    (advice-add 'completing-read :around #'stillness--handle-point)
     (advice-remove 'completing-read #'stillness--handle-point)))
 
 (provide 'stillness)
